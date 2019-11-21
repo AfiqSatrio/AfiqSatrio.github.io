075fd201c29e3d2aadd15bda17c7fec4fa22b022
= <=>
2019-11-21 09:16:15 +0700

    “UGD12_B_9426”

---
+++ precache-manifest.ff9379fa4dfc1a3a3c12fbbfbef88988.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95dddef669a69e1ab78a",
+    "url": "/css/chunk-vendors.1daa71b9.css"
+  },
+  {
+    "revision": "63a7d78d42c33b94fc7b957524795cac",
+    "url": "/img/logo.63a7d78d.svg"
+  },
+  {
+    "revision": "dff545ee0c5ccd96b4cdad36a3f90901",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4d4822ea5f5f999f9b6f",
+    "url": "/js/app.09c794c3.js"
+  },
+  {
+    "revision": "95dddef669a69e1ab78a",
+    "url": "/js/chunk-vendors.e40301e6.js"
+  },
+  {
+    "revision": "a12373dbf570402cc5dde830f8b7512f",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
